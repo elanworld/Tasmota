ab1aabd9ae35106a355c2992c20be5f6dd692f34
alan <elanworld@163.com>
2025-04-15 11:24:09 +0800

    add manager in ap;restart itme modify

--- tasmota/tasmota_xdrv_driver/xdrv_01_9_webserver.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_01_9_webserver.ino
@@ -721,7 +721,7 @@ void WifiManagerBegin(bool reset_only) {
   DnsServer->setErrorReplyCode(DNSReplyCode::NoError);
   DnsServer->start(DNS_PORT, "*", WiFi.softAPIP());
 
-  StartWebserver((reset_only ? HTTP_MANAGER_RESET_ONLY : HTTP_MANAGER));
+  StartWebserver((reset_only ? HTTP_MANAGER_RESET_ONLY : HTTP_ADMIN));
 }
 
 /*-------------------------------------------------------------------------------------------*/
